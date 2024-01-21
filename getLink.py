edf25793f17337b4ec175c5278ebe642722a8b2f
one <imone075@gmail.com>
2024-01-21 13:42:10 +0900

    initialize repo

---
+++ getLink.py
@@ -0,0 +1,75 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import json
+from random import randrange
+from datetime import datetime
+from time import sleep
+
+from config import CATEGORY_DICT, KEYWORD_XPATH, CATEGORY_XPATH
+
+options = webdriver.ChromeOptions()
+options.add_argument('window-size=1920,1080')
+
+def getLinkByKeyword(keyword, pages):
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(5)
+
+    result = []
+
+    for page in range(pages):
+        driver.get(url="https://kin.naver.com/search/list.naver?query=" + keyword + "&page=" + str(page + 1))
+
+        q_list = driver.find_elements(By.XPATH, KEYWORD_XPATH)
+
+        for q in q_list:
+            result.append(
+                {
+                    "id": q.get_attribute("href").split("&docId=")[-1].split("&")[0],
+                    "title": q.text,
+                    "link": q.get_attribute("href")
+                }
+            )
+        sleep(0.3 + randrange(2, 8) / 10)
+
+    link_path = "./link/keyword_link/" + datetime.today().strftime("%y%m%d.%H%M%S.") + keyword + "link.json"
+
+    with open(link_path, 'w', encoding='utf-8') as file:
+        json.dump(result, file, ensure_ascii=False, indent=2)
+
+    driver.close()
+
+    return link_path
+
+def getLinkByCategory(category, pages):
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(5)
+
+    result = []
+
+    for page in range(pages):
+        driver.get(url="https://kin.naver.com/qna/kinupList.naver?dirId=" + str(CATEGORY_DICT[category]) + "&page=" + str(page + 1))
+
+        q_list = driver.find_elements(By.XPATH, CATEGORY_XPATH)
+
+        for q in q_list:
+            result.append(
+                {
+                    "id": q.get_attribute("href").split("&docId=")[-1].split("&")[0],
+                    "title": q.text,
+                    "link": q.get_attribute("href")
+                }
+            )
+        sleep(0.3 + (randrange(2, 8) / 10))
+
+    link_path = "./link/category_link/" + datetime.today().strftime("%y%m%d.%H%M%S.") + category + ".json"
+
+    with open(link_path, 'w', encoding='utf-8') as file:
+        json.dump(result, file, ensure_ascii=False, indent=2)
+
+    driver.close()
+
+    return link_path
+
+if __name__ == "__main__":
+    getLinkByCategory(1, 5)
---
+++ linkrawler.py
@@ -0,0 +1,24 @@
+from getLink import getLinkByCategory, getLinkByKeyword
+from getHtml import getHtml
+import argparse
+
+from config import CATEGORY_DICT
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", type=str, help="Choose getLink mode with \"category\" or \"keyword\".")
+    parser.add_argument("--category", type=str, default="", help="Input your category with category mode.\n" +
+                        "options: " + " | ".join(list(CATEGORY_DICT.keys())))
+    parser.add_argument("--keyword", type=str, default="", help="Input your category with keyword mode.")
+    parser.add_argument("--pages", type=int, default=1, help="Maximum pages to get link.")
+    parser.add_argument("--path", type=str, default="./html/", help="Choose directory to download full HTML.")
+    args = parser.parse_args()
+
+    if(args.mode == "category"):
+        link_name = getLinkByCategory(args.category, args.pages)
+        getHtml(link_name, args.category, args.path)
+    elif(args.mode == "keyword"):
+        link_name = getLinkByKeyword(args.keyword, args.pages)
+        getHtml(link_name, args.keyword, args.path)
+    else:
+        print("*** Alert: mode not selected. ***")
